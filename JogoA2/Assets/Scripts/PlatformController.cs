46fed381527f4dac0bb720665700fb8f620e38e6
Amanda Viceconte <amanda.viceconte@gmail.com>
2018-05-29 21:46:37 -0300

    First Commit

---
+++ JogoA2/Assets/Scripts/PlatformController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	[HideInInspector] public bool facingRight = true;
+	[HideInInspector] public bool jump = true;
+
+	public float moveForce = 365f;
+	public float maxSpeed = 5f;
+	public float jumpForce = 1000f;
+	public Transform groundCheck;
+
+	private bool grounded = false;
+	private Animator anim;
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
+		// Jump only if player is on the ground
+		if (Input.GetButtonDown ("Jump") && grounded) {
+			jump = true;
+		}
+	}
+
+	// Physics loop
+
+
+	void Flip() {
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= (-1);
+		transform.localScale = theScale;
+	}
+}
